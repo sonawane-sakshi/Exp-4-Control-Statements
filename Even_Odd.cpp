06534e37aeee449314235dd7b4a89433d8ac7670
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-04 16:29:57 +0530

    Add files via upload

---
+++ Even_Odd.cpp
@@ -0,0 +1,15 @@
+//Sakshi Sonawane
+//24070123505
+#include <iostream>
+using namespace std;
+int main()
+ {
+    int n;
+    cout<<"enter a no"<<endl;
+    cin>>n;
+    if (n%2==0)
+    cout<<"it is even no"<<endl;
+    else
+    cout<<"it is odd no"<<endl;
+    return 0;
+}
