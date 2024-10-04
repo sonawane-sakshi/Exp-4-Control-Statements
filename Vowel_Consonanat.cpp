afe42d434579d6ca2fe1f8693b1e4024ba6139ea
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-04 16:24:59 +0530

    Add files via upload

---
+++ Vowel_Consonanat.cpp
@@ -0,0 +1,23 @@
+//Sakshi Sonawane 
+//24070123505
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	int n,a,e,i,o,u,A,E,I,O,U;
+	cout<<"value of character:";
+	cin>>n;
+	if(n==a||n==e||n==i||n==o||n==u||n==A||n==E||n==I||n==O||n==U)
+	{
+		cout<<"character is a vowel";
+	}
+	
+	else
+	{
+		cout<<"character is a consonant"<<endl;
+		}
+		
+	}
