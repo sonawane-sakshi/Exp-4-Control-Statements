5b35644925f6e3de879d2300be6885d9e42d9e1b
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-04 16:33:36 +0530

    Add files via upload

---
+++ Largest No.cpp
@@ -0,0 +1,25 @@
+//Sakshi Sonawane 
+//24070123505
+//exp5-largest of two numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter 1st number: ";
+    cin>>a;
+    cout<<"Enter 2nd number: ";
+    cin>>b;
+    if(a>b)
+    {
+        cout<<a<<" is greater than "<<b;
+    
+    }
+    else
+    {
+        cout<<b<<" is greater than "<<a;
+
+    }
+    return 0;
+}
+
